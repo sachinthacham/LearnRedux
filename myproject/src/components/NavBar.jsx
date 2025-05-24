f04a6438107be2bd851435a13132fe7f9a4d7380
sachinthacham <sachinthachamindu26@gmail.com>
2025-05-24 23:13:47 +0530

    implement navigation  bar

--- myproject/src/components/NavBar.jsx
+++ myproject/src/components/NavBar.jsx
@@ -1,11 +1,13 @@
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 const Navbar = () => {
-  const navLinks = [
-    { name: "Dashboard", href: "/" },
-    { name: "Products", href: "/products" },
-    { name: "Pricing", href: "/pricing" },
-    { name: "About", href: "/about" },
-  ];
+  const navigate = useNavigate();
 
+  const handleNavigate = () => {
+    navigate("/cart");
+  };
+
+  const cartProducts = useSelector((state) => state.cart);
   return (
     <nav className="bg-white shadow-md w-full">
       <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
@@ -14,22 +16,7 @@ const Navbar = () => {
         </a>
 
         <div className="flex items-center space-x-8">
-          {navLinks.map((link) => (
-            <a
-              key={link.name}
-              href={link.href}
-              className="text-gray-700 hover:text-indigo-600 transition font-medium"
-            >
-              {link.name}
-            </a>
-          ))}
-
-          <a
-            href="/login"
-            className="bg-indigo-600 text-white px-4 py-2 rounded-md hover:bg-indigo-700 transition"
-          >
-            Login
-          </a>
+          <button onClick={handleNavigate}>My Bag {cartProducts.length}</button>
         </div>
       </div>
     </nav>
