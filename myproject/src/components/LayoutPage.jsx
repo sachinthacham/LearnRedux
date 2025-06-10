b60df7ba9fa3bad97ab6a6a61f131ec37f0c69d1
sachinthacham <sachinthachamindu26@gmail.com>
2025-06-10 09:14:23 +0530

    implement layout for the ecommerce app

--- myproject/src/components/LayoutPage.jsx
+++ myproject/src/components/LayoutPage.jsx
@@ -4,12 +4,12 @@ import { Provider } from "react-redux";
 import store from "../store/Store";
 import { Outlet } from "react-router-dom";
 
-const LayoutPage = () => {
+const LayoutPage = ({ darkMode, setDarkMode }) => {
   return (
     <Provider store={store}>
-      <div>
-        <Navbar />
-        <main>
+      <div className="min-h-screen bg-gray-100 dark:bg-gray-900 transition-colors duration-200">
+        <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
+        <main className="container mx-auto px-4 py-8">
           <Outlet />
         </main>
       </div>
