6ed688ebd34952e95acd9a7a45ff2a59d634b075
sachinthacham <sachinthachamindu26@gmail.com>
2025-06-10 09:17:21 +0530

    implement cardslice for add/ remove items

--- myproject/src/store/CardSlice.jsx
+++ myproject/src/store/CardSlice.jsx
@@ -8,12 +8,11 @@ const cartSlice = createSlice({
     add: (state, action) => {
       state.push(action.payload);
     },
-    // removeFromCart: (state, action) => {
-    //     return state.filter(item => item.id !== action.payload.id);
-    // },
-    // clearCart: () => initialState,
+    remove: (state, action) => {
+      return state.filter((item) => item.id !== action.payload);
+    },
   },
 });
 
-export const { add } = cartSlice.actions;
+export const { add, remove } = cartSlice.actions;
 export default cartSlice.reducer;
