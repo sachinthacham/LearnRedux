a35674599d7fb1fbad8dd59fa09aab63b804703f
sachinthacham <sachinthachamindu26@gmail.com>
2025-06-10 09:14:56 +0530

    update the code for dark mode

--- myproject/src/App.jsx
+++ myproject/src/App.jsx
@@ -1,16 +1,32 @@
+import { useEffect, useState } from "react";
 import LayoutPage from "./components/LayoutPage";
 import { Routes, Route } from "react-router-dom";
 import Product from "./components/Product";
 import Cart from "./components/Cart";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [darkMode]);
+
   return (
-    <Routes>
-      <Route path="/" element={<LayoutPage />}>
-        <Route index element={<Product />} />
-        <Route path="cart" element={<Cart />} />
-      </Route>
-    </Routes>
+    <div className="min-h-screen bg-gray-100 dark:bg-gray-900 transition-colors duration-200">
+      <Routes>
+        <Route
+          path="/"
+          element={<LayoutPage darkMode={darkMode} setDarkMode={setDarkMode} />}
+        >
+          <Route index element={<Product />} />
+          <Route path="cart" element={<Cart />} />
+        </Route>
+      </Routes>
+    </div>
   );
 }
 
